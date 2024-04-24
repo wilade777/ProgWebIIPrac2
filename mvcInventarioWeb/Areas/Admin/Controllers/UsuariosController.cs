d48289c4abb1d83e5d581749373c28489c3fd357
wilade777 <51494119+wilade777@users.noreply.github.com>
2024-04-24 19:07:49 -0400

    Add files via upload

---
+++ mvcInventarioWeb/Areas/Admin/Controllers/UsuariosController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvcInventarioWeb.AccesoDatos.Data.Repository.IRepository;
+using mvcInventarioWeb.Models;
+using System.Security.Claims;
+
+namespace mvcInventarioWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsuariosController : Controller
+    {
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+
+        public UsuariosController(IContenedorTrabajo contenedorTrabajo)
+        {
+            _contenedorTrabajo = contenedorTrabajo;
+        }
+        //[HttpGet]
+        //public IActionResult Index()
+        //{
+        //    //opcion 1 : obtener todos los usuarios
+        //    return View(_contenedorTrabajo.Usuario.GetAll());
+        //}
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //opcion 2 : obtener todos los usuarios menos el que esta logueado
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var usuarioActual = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            return View(_contenedorTrabajo.Usuario.GetAll(u => u.Id != usuarioActual.Value));
+        }
+        [HttpGet]
+        public IActionResult Bloquear(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var usuarioActual = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            _contenedorTrabajo.Usuario.BloquearUsuario(id);
+            return View("Index",_contenedorTrabajo.Usuario.GetAll(u => u.Id != usuarioActual.Value));
+        }
+        [HttpGet]
+        public IActionResult Desbloquear(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var usuarioActual = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            _contenedorTrabajo.Usuario.Desbloquearusuario(id);
+            return View("Index", _contenedorTrabajo.Usuario.GetAll(u => u.Id != usuarioActual.Value));
+        }
+    }
+}
